5a474ca871cd91fef0fb6291b71199e4f83a433f
hefujie <hefujie@msn.cn>
2021-01-17 17:52:33 +0800

    InitialProject

---
+++ UCustomMadeMovies/UCustomMadeMovies/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using UCustomMadeMovies.ViewModels;
+using Xamarin.Forms;
+
+namespace UCustomMadeMovies.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ UCustomMadeMovies/UCustomMadeMovies/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UCustomMadeMovies.Models;
+using UCustomMadeMovies.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UCustomMadeMovies.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
